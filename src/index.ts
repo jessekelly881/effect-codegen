a92c3e173534044d86a98af030677e0744ca4c9d
Jesse Kelly <kellyjesse881@gmail.com>
2023-08-19 15:56:04 -0600

    init

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,15 @@
-/**
- * @since 1.0.0
- */
-export const index = 1
+import * as Effect from "@effect/io/Effect";
+import * as Fs from "@effect/platform-node/FileSystem"
+import { pipe } from "@effect/data/Function";
+
+
+const readFile = pipe(
+    Fs.FileSystem,
+    Effect.flatMap((fs) => fs.readFile(`${__dirname}/test.json`)),
+    Effect.map(s => new TextDecoder().decode(s)),
+    Effect.flatMap(s => Effect.log(s)),
+    Effect.provideLayer(Fs.layer),
+    Effect.catchAll(s => Effect.logError("Err"))
+)
+
+Effect.runPromise(readFile)
